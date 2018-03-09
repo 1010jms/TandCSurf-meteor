2fcaad56e36154b84d0ee6d15dedeb5d4de5b8b0
1010jms <1010jms@gmail.com>
2018-03-08 21:00:23 -1000

    Completed T&C Surf Mockup in Meteor

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -4,24 +4,13 @@ import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
 import TandCSurf from '../pages/TandCSurf';
 import TandCLogo from '../components/TandCLogo';
-import Est1971 from '../components/Est1971';
 import SocialMediaMenu from '../components/SocialMediaMenu';
 import TopMenu from '../components/TopMenu';
 import FooterMenu from '../components/FooterMenu';
 import FooterImage from '../components/FooterImage';
 import NotFound from '../pages/NotFound';
-import Signin from '../pages/Signin';
-import Signup from '../pages/Signup';
-import Signout from '../pages/Signout';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
